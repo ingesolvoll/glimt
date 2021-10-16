1236a8ca7297a1825fbfeeaaf862b1ea02adc1db
ingesolvoll <inge.solvoll@gmail.com>
2021-10-16 10:35:57 +0200

    Local install

--- build.clj
+++ build.clj
@@ -4,7 +4,13 @@
 
 (def lib 'glimt/glimt)
 ;; if you want a version of MAJOR.MINOR.COMMITS:
-(def version "0.1.0")
+(def version "0.1.1")
+
+(defn install [opts]
+      (-> opts
+          (assoc :lib lib :version version)
+          (bb/jar)
+          (bb/install)))
 
 (defn deploy [opts]
       (-> opts
